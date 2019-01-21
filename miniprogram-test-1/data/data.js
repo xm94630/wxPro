ab94f05c285d2bce123e27b8641766557b9be819
xm94630 <xm94630@126.com>
2019-01-21 19:56:01 +0800

    构建基础

---
+++ miniprogram-test-1/data/data.js
@@ -0,0 +1,24 @@
+var local_database = [
+  {
+    "date": "2019",
+    "title": "我是标题哦",
+    "post_img": "/images/3.jpeg",
+    "author_img": "/images/avatar/avatar.png",
+    "content": "这里风景很不错啊",
+    "view_num": "123",
+    "collect_num": "99",
+  },
+  {
+    "date": "2020",
+    "title": "我是标题哦",
+    "post_img": "/images/2.jpeg",
+    "author_img": "/images/avatar/avatar.png",
+    "content": "我也很想去哦",
+    "view_num": "38",
+    "collect_num": "85",
+  },
+]
+
+module.exports = {
+  postList:local_database
+}
