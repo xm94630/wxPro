d228797ba59e6cd9f9fad5def26ce68a9fe746cd
xm94630 <xm94630@126.com>
2019-01-22 15:03:01 +0800

    6-7 收藏

--- miniprogram-test-1/pages/posts/posts-detail/posts-detail.js
+++ miniprogram-test-1/pages/posts/posts-detail/posts-detail.js
@@ -1,13 +1,45 @@
 var myData = require("../../../data/data.js")
 Page({
-  onLoad:function(opts){
+  onLoad: function(opts) {
     var id = opts.id;
+    
+    this.setData({
+      id: id,
+      postData: myData.postList[id],
+    })
+
 
     //老版本，这个是可以用的（同步代码）。现在不行了
     //this.data.postData = myData.postList[id];
-    
+
+    var postCollected = wx.getStorageSync("postCollected");
+    if (postCollected) {
+      var collect = postCollected[id];
+      this.setData({
+        collected: collect
+      })
+    } else {
+      postCollected = {}
+      //postCollected[id] = false;
+      wx.setStorageSync("postCollected", postCollected)
+    }
+
+  },
+
+  onCollectionTap: function (event) {
+
+    var id = this.data.id;
+    var postCollected = wx.getStorageSync("postCollected");
+    var collect = postCollected[id];
+    //切换
+    collect = !collect;
+    postCollected[id] = collect
+    //更新缓存
+    wx.setStorageSync("postCollected", postCollected)
+    //更新数据
     this.setData({
-      postData: myData.postList[id]
+      collected: collect
     })
-  }
+
+  },
 })
