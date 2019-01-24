89f9912d67938ca0587bd3fc9a5de7ed8e9334ff
xm94630 <xm94630@126.com>
2019-01-24 21:46:37 +0800

    7-13 接口数据

--- miniprogram-test-1/pages/movies/movies.js
+++ miniprogram-test-1/pages/movies/movies.js
@@ -1,66 +1,64 @@
-// pages/movies/movies.js
+var app=getApp();
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    var inTheatersUrl  = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
+
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getMovieListData:function(url){
+    var that = this;
+    wx.request({
+      url: url,
+      method:"GET",
+      header:{
+        "Content-Type":""
+      },
+      success:function(data){
+        that.processDoubanData(data.data);
+      },
+      fail:function(){
 
+      },
+      complete:function(){
+
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  processDoubanData:function(moviesDouban){
+    var movies = [];
+    for(var idx in moviesDouban.subjects){
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if(title.length>=6){
+        title = title.substring(0,6)+"...";
+      }
+      var temp= {
+        title:title,
+        average:subject.rating.average,
+        coverageUrl:subject.images.large,
+        movieId:subject.id
+      }
+      movies.push(temp)
+    }
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    this.setData({
+      movies:movies
+    })
 
   }
+
 })
+
